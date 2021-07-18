59c6fbe2726c254e88c1acd3d25330a7d9d9d097
shubharthaksangharsha <shubharthaksangharsha>
2021-07-18 12:46:46 +0530

    Added save button and save function

--- passenger_counter/index.js
+++ passenger_counter/index.js
@@ -1,11 +1,16 @@
-//document.getElementById("count-el").innerText = 5
-
 let countEl = document.getElementById("count-el")
-
+let saveEl = document.getElementById("save-el")
 let count = 0
 function increment(){
-    count = count + 1
+    count += 1
     countEl.innerText = count
     
 }
+
+function save(){
+    let showCount = count + " - "
+    saveEl.innerText += showCount
+}
+
+
 
