72538b01a5043557d0d720e77b11d76eb56b2a51
shubharthaksangharsha <shubharthaksangharsha>
2021-07-18 14:31:34 +0530

    Decorated the project

--- passenger_counter/index.js
+++ passenger_counter/index.js
@@ -1,16 +1,22 @@
 let countEl = document.getElementById("count-el")
 let saveEl = document.getElementById("save-el")
+let resetEl = document.getElementById("reset-btn")
 let count = 0
 function increment(){
     count += 1
-    countEl.innerText = count
-    
+    countEl.textContent = count
+   
 }
 
 function save(){
     let showCount = count + " - "
     saveEl.textContent += showCount
+    countEl.textContent = 0
+    count = 0
 }
 
-
-
+function reset(){
+    saveEl.textContent = "Previous entires: ";
+    count = 0;
+    countEl.textContent = 0
+}
